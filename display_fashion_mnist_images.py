c5347360538d6f24cecd02a3715cfc8e20eafd01
sumit bawane <sumitsr29@gmail.com>
2025-03-04 00:31:38 +0530

    added image for each class and logged those images to wandb

--- display_fashion_mnist_images.py
+++ display_fashion_mnist_images.py
@@ -1,34 +1,20 @@
-import matplotlib.pyplot as plt
-from keras.datasets import fashion_mnist
-import numpy as np
 import wandb
-
-# Load the Fashion MNIST dataset
-(x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
-
-# Define class labels
-class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
-               'sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+import numpy as np
+import pandas as pd
+import keras 
 
 # Initialize WandB
-wandb.init(project="da6401-Assignemt1")
+wandb.init(project="da6401-Assignment1")
 
-# Create a grid to plot sample images for each class
-fig, axes = plt.subplots(nrows=2, ncols=5, figsize=(10, 5))
-axes = axes.flatten()
+# Load dataset
+(x_train, y_train), (x_test,y_test) = keras.datasets.fashion_mnist.load_data()
 
-# Iterate through each class
-for i in range(10):
-    # Find index of the first image for the current class
-    class_idx = np.where(y_train == i)[0][0]
-    img = x_train[class_idx]
+# Convert to DataFrame
+df = pd.DataFrame({"image": list(x_train), "label": y_train})
 
-    # Plot the image
-    axes[i].imshow(img, cmap='gray')
-    axes[i].set_title(class_names[i])
-    axes[i].axis('off')
+for label, group in df.groupby("label"):
+    image = group.iloc[0]["image"]  # Get the first image of each class
+    wandb.log({"fashion_mnist_samples": wandb.Image(image, caption=f"Label {label}")})
 
-# Log the plot to WandB
-wandb.log({"sample_images": wandb.Image(fig)})
-
-plt.show()
+# Finish WandB run
+wandb.finish()
